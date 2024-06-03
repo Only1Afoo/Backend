f7f9edc2627008618926c322478a58e5bfc9a6d6
Only1Afoo <sheriffdeenolawale@gmail.com>
2024-06-03 04:19:39 -0700

    Express.js first lesson

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const port = 3002;
+app.get("/", (req, res) => {
+    res.send('Hello world!');
+});
+app.listen(port, () => console.log(`Listening tp port ${port}`));
+
